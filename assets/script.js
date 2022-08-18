965a75d240b73e8a4d73eda4f91cf28ffd2b6dff
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-08-17 18:23:46 -1000

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,41 @@
 //Today's date
-//
-var currentDate = moment().format(dd MMMM Do YY);
+//variable to store the date and format
+var now = moment().hour();
+var currentDate = moment().format('DDDD, MMMM, Do, YYYY');
+//link to html currentday
 $("#currentDay").html(currentDate);
+  
 
 
-//current time
+
  
 //save button
 var saveButton = $('saveBtn');
 saveButton.on('click', function() {
-    var hour = 
+    //create variable to store the time of the entered data
+    var hour = $(this).siblings(".timeColumn").val();
+    //create variable to store the text in the text area
+    var info = $(this).siblings(".columnText").text();
+    //set local storage to the time and text
+    localStorage.setItem(hour, info);
 })
+
+//keep saved items on the planner
+//var test = $("#7 .columnText").text(localStorage.getItem('7'));
+//console.log(test);
+$("#7 .columnText").text(localStorage.getItem('7'));
+$("#8 .columnText").text(localStorage.getItem('8'));
+$("#9 .columnText").text(localStorage.getItem('9'));
+$("#10 .columnText").text(localStorage.getItem('10'));
+$("#11 .columnText").text(localStorage.getItem('11'));
+$("#12 .columnText").text(localStorage.getItem('12'));
+$("#1 .columnText").text(localStorage.getItem('1'));
+$("#2 .columnText").text(localStorage.getItem('2'));
+$("#3 .columnText").text(localStorage.getItem('3'));
+$("#4 .columnText").text(localStorage.getItem('4'));
+
+//try to get blocks to change color depending on the time
+function blockColor() {
+
+}
+
