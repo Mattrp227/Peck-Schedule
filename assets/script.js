29d15255a09a27333627b73257570dd80e3a905f
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-08-17 17:40:22 -1000

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
+//Today's date
+//
+var currentDate = moment().format(dd MMMM Do YY);
+$("#currentDay").html(currentDate);
 
+
+//current time
+ 
+//save button
+var saveButton = $('saveBtn');
+saveButton.on('click', function() {
+    var hour = 
+})
